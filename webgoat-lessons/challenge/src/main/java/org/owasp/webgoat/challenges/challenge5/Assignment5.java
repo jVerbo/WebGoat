71103c2945419cadee7fa23759e4fe4637864c0c
Jesús Verbo <j.verbo.2018@alumnos.urjc.es>
2021-03-15 10:37:03 +0100

    Fixing Sql Injection Vulnerability

--- webgoat-lessons/challenge/src/main/java/org/owasp/webgoat/challenges/challenge5/Assignment5.java
+++ webgoat-lessons/challenge/src/main/java/org/owasp/webgoat/challenges/challenge5/Assignment5.java
@@ -56,7 +56,10 @@ public class Assignment5 extends AssignmentEndpoint {
             return failed(this).feedback("user.not.larry").feedbackArgs(username_login).build();
         }
         try (var connection = dataSource.getConnection()) {
-            PreparedStatement statement = connection.prepareStatement("select password from challenge_users where userid = '" + username_login + "' and password = '" + password_login + "'");
+            String sql = "select password from challenge_users where userid = ?, and password = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, username_login);
+            statement.setString(2, password_login);
             ResultSet resultSet = statement.executeQuery();
 
             if (resultSet.next()) {
